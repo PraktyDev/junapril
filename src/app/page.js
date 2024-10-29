0093e770989ce50f27fefa95e26ef8b5bda536e2
Praise <prakty1205@gmail.com>
2024-10-29 10:03:28 -0600

    added text carousel for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,44 @@
+"use client"
 import ImageCard from "@/components/ImageCard"
 import ServiceCard from "@/components/ServiceCard"
 import ServiceLinks from "@/components/ServiceLinks"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 const page = () => {
+    const texts = [
+      "Welcome to Junapril Solutions Ltd",
+      "Compliance Consulting for Business",
+      "Health Privacy Record",
+    ];
+  
+    const [currentTextIndex, setCurrentTextIndex] = useState(0)
+    const [fade, setFade] = useState(true)
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setFade(false)
+        setTimeout(() => {
+          setCurrentTextIndex((prev) => (prev + 1) % texts.length)
+          setFade(true)
+        }, 300)
+      }, 5000)
+  
+      return () => clearInterval(interval)
+    }, [texts.length]);
+
   return (
     <section className='flex flex-col justify-center gap-4 w-full'>
       <div className="relative flex items-center justify-center laptop:justify-end w-full h-[800px] bg-[url('/images/homehero.png')] bg-no-repeat">
           <div className='bg-black opacity-40 w-full h-[800px] absolute top-0'></div>
           <div className='z-10 flex flex-col justify-center laptop:items-end laptop:pr-10 items-center gap-14 '> 
-            <h1 className='text-4xl tablet:text-5xl laptop:text-6xl text-white text-center font-bold'>Welcome <br />to <br />Junapril Solutions Ltd</h1>
+            <h1
+              className={`text-4xl tablet:text-5xl laptop:text-6xl w-full laptop:w-[700px] text-white text-center font-bold transition-opacity duration-500 ${
+                fade ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              {texts[currentTextIndex]}
+            </h1>
             <p className='laptop:-mt-8 mx-4 tablet:mx-0 text-md laptop:text-lg text-center font-semibold text-white'>Your trusted partner in IT security consulting, recruitment, and eCommerce solutions.</p>
             <Link href={'/contact'} className='w-40 laptop:w-56 laptop:-mt-8 text-nowrap text-center bg-none hover:bg-[#277A80] text-white text-md tablet:text-lg laptop:text-xl border-2 px-2 laptop:px-10 py-1.5 laptop:py-2 border-[#277A80] rounded-xl'>Get in Touch</Link>
           </div>
@@ -22,8 +51,8 @@ const page = () => {
         <ImageCard src='/images/ecom.png' text='E-COMMERCE' alt='ecommerce' />
       </div>
 
-      <div className="w-full relative flex flex-col items-center laptop:items-start h-[600px] bg-[url('/images/homehero2.png')] bg-no-repeat">
-        <div className='bg-black opacity-40 w-full h-[600px] absolute top-0'></div>
+      <div className="w-full relative flex flex-col items-center laptop:items-start h-[600px] bg-no-repeat animate-home2BgSlide">
+        <div className='bg-black opacity-50 w-full h-[600px] absolute top-0'></div>
         <div className='z-10 w-full laptop:w-[500px] mb-16 flex items-center justify-center shadow-sm rounded-sm laptop:ml-5 bg-gradient-to-r from-teal-700/95 via-teal-700/50 to-teal-700/40'>
           <p className='text-white text-center text-lg laptop:text-xl font-bold py-4'>OUR SERVICES</p>
         </div>
